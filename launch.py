104a0a7bef0283f37a27db35c831e11f5b084119
Amir Giveon <agiveon@gmail.com>
2021-09-27 09:07:56 -0700

    Update launch.py

--- launch.py
+++ launch.py
@@ -21,16 +21,6 @@ def classify_art(img):
     new_pred = model(img.view(1,3,224,224)).argmax()
   return labels[new_pred.item()]
   
-def classify_art_desc(img):
-
-  img = transforms.Resize((224,224))(img)
-  img = transforms.ToTensor()(img)
-  img = transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])(img)
-
-  with torch.no_grad():
-    new_pred = model(img.view(1,3,224,224)).argmax()
-  return type_desc[new_pred.item()]
-
 image = gr.inputs.Image(type='pil',image_mode="RGB")
 label = gr.outputs.Label()
 title = 'What type of artist is your child?'
