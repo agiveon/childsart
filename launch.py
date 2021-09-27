dd43d5ee17c6adbf01cbb425eccb2009afaef552
Amir Giveon <agiveon@gmail.com>
2021-09-27 09:05:47 -0700

    Update launch.py

--- launch.py
+++ launch.py
@@ -9,7 +9,9 @@ model.eval()
 
 labels = ['ABSTRACT ART', 'CONTEMPORARY ART', 'CUBISM', 'FANTASY ART', 'GRAFFITI', 'IMPRESSIONISM', 'POP ART', 'SURREALISM']
 
-def classify_dino(img):
+type_desc = ['desc1','desc2','desc3','desc4','desc5','desc6','desc7','desc8']
+
+def classify_art(img):
 
   img = transforms.Resize((224,224))(img)
   img = transforms.ToTensor()(img)
@@ -18,6 +20,16 @@ def classify_dino(img):
   with torch.no_grad():
     new_pred = model(img.view(1,3,224,224)).argmax()
   return labels[new_pred.item()]
+  
+def classify_art_desc(img):
+
+  img = transforms.Resize((224,224))(img)
+  img = transforms.ToTensor()(img)
+  img = transforms.Normalize([0.485, 0.456, 0.406],[0.229, 0.224, 0.225])(img)
+
+  with torch.no_grad():
+    new_pred = model(img.view(1,3,224,224)).argmax()
+  return type_desc[new_pred.item()]
 
 image = gr.inputs.Image(type='pil',image_mode="RGB")
 label = gr.outputs.Label()
@@ -27,6 +39,4 @@ description = 'Is your child more of an impressionist or contemporary art kinda 
 
 sample_images = [["ex1.JPG"],['ex2.JPG'],['ex3.JPG'],['ex4.JPG']]
 
-description = 'Is your child more of an impressionist or contemporary? Surrialist maybe? Snap a picture of your child\'s art and find out what genre is their art!'
-
-gr.Interface(fn=classify_dino, inputs=image, outputs=label, capture_session=True, examples=sample_images, title=title, description=description).launch(share=True)
+gr.Interface(fn=classify_art, inputs=image, outputs=label, capture_session=True, examples=sample_images, title=title, description=description).launch(share=True)
